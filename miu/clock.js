a8abee3588ed7f92e79fdb1c3a32e3a800a37427
Sandra Untersander <suntersander@salesforce.com>
2024-05-31 18:53:12 -0300

    moved to subfolder

--- miu/clock.js
+++ miu/clock.js
@@ -67,7 +67,7 @@ class CountdownTimer {
 // setup timer with set textual date in the future
 const timer1 = new CountdownTimer({
     selector: "#clock1",
-    targetDate: new Date("April, 13 2024 21:00:00"),
+    targetDate: new Date("April, 13 2025 21:00:00"),
 });
 
 timer1.startTimer();
